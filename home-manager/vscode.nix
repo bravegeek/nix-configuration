b844711568bc6837dd25d7793dd70cdd877179c6
Greg Read <gregread@gmail.com>
2023-10-21 15:34:50 -0400

    vscode again

--- home-manager/vscode.nix
+++ home-manager/vscode.nix
@@ -5,13 +5,22 @@
 {
   programs.vscode = {
     enable = true;
+
+    # package = (pkgs.vscode.override{ isInsiders = true; }).overrideAttrs (oldAttrs: rec {
+    #   src = (builtins.fetchTarball {
+    #     url = "https://update.code.visualstudio.com/latest/linux-x64/insider";
+    #     sha256 = "0djmjf7d10qin90rx4r83aim9dlaihasgl1a6qbq6sil5dynp2s9";
+    #   });
+    #   version = "latest";
+    # });
+
     extensions = with pkgs.vscode-extensions; [
       dracula-theme.theme-dracula
       jnoortheen.nix-ide
-      vscode-icons-team.vscode-icons
-      esbenp.prettier-vscode
-      ms-dotnettools.csharp
-      ionide.ionide-fsharp
+      # vscode-icons-team.vscode-icons
+      # esbenp.prettier-vscode
+      # ms-dotnettools.csharp
+      # ionide.ionide-fsharp
       # eamodio.gitlens
       # vscodevim.vim
       # yzhang.markdown-all-in-one
